05aa543c00d547fb56f71d4b8129fa8038086952
Kylapurity <kihiupurity29@gmail.com>
2023-10-11 13:35:16 -0700

    0x1E-search_algorithms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,54 +1,55 @@
 #include "search_algos.h"
 
 /**
- *binary_search - searches an array using binary search algorithm
+ * print_array - prints an array of integers
+ * @array: array to print
+ * @size: size of array
  *
- *@array: The array being searched
- *@size: The size of the array being searched
- *@value: The value being searched for
- *Return: returns the index of the value found
+ * return: void
  */
-int binary_search(int *array, size_t size, int value)
+void print_array(int *array, size_t size)
 {
-	size_t L = 0;
-	size_t R = size - 1;
-	size_t m;
+	size_t i;
 
-	if (!array)
-		return (-1);
-
-	while (L <= R)
+	printf("Searching in array:");
+	for (i = 0; i < size; i++)
 	{
-		print_array(array, L, R);
-		m = ((L + R) - ((L + R) % 2)) / 2;
-		if (array[m] < value)
-			L = m + 1;
-		else if (array[m] > value)
-			R = m - 1;
-		else
-			return (m);
+		printf(" %d", array[i]);
+		if (i != size - 1)
+			printf(",");
 	}
-	return (-1);
+	printf("\n");
 }
 
 /**
- *print_array - custom print function for printing an array
+ * binary_search - searches for a value in a sorted array of integers using the
+ * Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
  *
- *@array: The array to print from
- *@index: The start index of the list
- *@boundary: The end index of the array
- *Return: void
+ * Return: index where value is located, or -1 on failure
  */
-void print_array(int *array, size_t index, size_t boundary)
+int binary_search(int *array, size_t size, int value)
 {
-	printf("Searching in array: ");
-	while (index <= boundary)
+	size_t l, m, r;
+
+	if (array != NULL && size > 0)
 	{
-		printf("%d", array[index]);
-		if (index != boundary)
-			printf(", ");
-		else
-			printf("\n");
-		index++;
+		l = 0;
+		r = size - 1;
+		print_array(array + l, r + 1 - l);
+		while (l < r)
+		{
+			m = (l + r) / 2;
+			if (array[m] < value)
+				l = m + 1;
+			else if (array[m] > value)
+				r = m;
+			else
+				return (m);
+			print_array(array + l, r + 1 - l);
+		}
 	}
+	return (-1);
 }
